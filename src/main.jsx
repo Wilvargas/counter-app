509ad48f9f9ca00bb7ea9609f1ee53919926ee48
Wilfredo <wilvargas7@gmail.com>
2024-08-13 13:31:36 -0500

    First Commit: Preparación de entorno de mi app, limpieza de fileSystem, creación de functinal components, emparejamiento con boostrap CSS

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// import { HooksApp } from './HooksApp';
+
+import { CounterApp } from './01-useState/CounterApp';
+
+import './index.css';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <CounterApp />
+  </StrictMode>,
+)
