29c4b3d880a50f98f4a05dade307ed108924248c
Wilfredo <wilvargas7@gmail.com>
2024-08-15 15:39:49 -0500

    Creación de useFetch y del functional component MultipleCustomHooks.jsx

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useFetch } from '../hooks'
+
+
+export const MultipleCustomHooks = () => {
+
+  const { data, hasError, isLoading } = useFetch('https://pokeapi.co/api/v2/pokemon/3');
+  
+  
+
+return (
+    <>
+    
+        <h1>Información de Pokemon</h1>
+        <hr />
+
+        { isLoading && <p>Cargando...</p> }
+
+        <h2>{ data?.name }</h2>
+    </>
+  )
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,15 @@ import { createRoot } from 'react-dom/client';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 
 import './index.css';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FormWithCustomHook />
+    {/* <FormWithCustomHook /> */}
+    <MultipleCustomHooks />
   </StrictMode>,
 )
