d74f845b421b03e335b87c865329fd0d8ff9972b
Wilfredo <wilvargas7@gmail.com>
2024-08-15 11:28:56 -0500

    Creación del button onResetForm que regresa el formulario a sus valores iniciales

--- src/02-useEffect/FormWithCustomHook.jsx
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -3,7 +3,7 @@ import { useForm } from "../hooks/useForm";
  
 export const FormWithCustomHook = () => {
 
-    const {formState, onInputChange, username, email, password} = useForm(  {
+    const {formState, onInputChange, username, email, password, onResetForm} = useForm(  {
         username:'', 
         email: '', 
         password: ''
@@ -12,35 +12,37 @@ export const FormWithCustomHook = () => {
     // const { username, email, password } = formState;
 
 
-  return (
-    <> 
-        <h1>Formulario con Custom Hook</h1>
-        <hr />
-        <input 
-           type="text" 
-           className="form-control"
-           placeholder="Username"
-           name="username"
-           value={ username }
-           onChange={ onInputChange }
-        />
-        <input 
-           type="email" 
-           className="form-control mt-2 mb-2"
-           placeholder="yosef@gmail.com"
-           name="email"
-           value={ email }
-           onChange={ onInputChange }
-        />
-        <input 
-           type="password" 
-           className="form-control mt-2 mb-2"
-           placeholder="contraseña"
-           name="password"
-           value={ password }
-           onChange={ onInputChange }
-        />
+   return (
+     <> 
+         <h1>Formulario con Custom Hook</h1>
+         <hr />
+         <input 
+            type="text" 
+            className="form-control"
+            placeholder="Username"
+            name="username"
+            value={ username }
+            onChange={ onInputChange }
+         />
+         <input 
+            type="email" 
+            className="form-control mt-2 mb-2"
+            placeholder="yosef@gmail.com"
+            name="email"
+            value={ email }
+            onChange={ onInputChange }
+         />
+         <input 
+            type="password" 
+            className="form-control mt-2 mb-2"
+            placeholder="contraseña"
+            name="password"
+            value={ password }
+            onChange={ onInputChange }
+         />
 
-    </>
-  );
-};
+         <button onClick={ onResetForm } className="btn btn-primary mt-2">Borrar</button>
+ 
+     </>
+   );
+}; 
