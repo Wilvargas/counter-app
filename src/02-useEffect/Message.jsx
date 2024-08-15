f21bbc920ff6a3b524cd65fabf45c665ae8d2969
Wilfredo <wilvargas7@gmail.com>
2024-08-14 23:32:39 -0500

    Dependencias del useEffect, useEffectunmount-Cleanup, useEffect - Precauciones

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,19 +1,28 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 export const Message = () => {
 
-    useEffect(() => {
-      console.log('Message Mounted');
-       
-      return () => {
-        console.log('Message Unmounted');
-      }
-    }, [])
+  const [coords, setCoords] = useState({ x:0, y:0 });
+
+  const onMouseMove = ({ x, y }) => {
+    // const coords = { x, y };
+    setCoords({ x, y });
+    
+  }
+  useEffect(() => {
+    
+    window.addEventListener('mousemove', onMouseMove); 
+    
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove); 
+    }
+  }, [])
     
   return (
     <>
         <h3>Usuario ya existe</h3>
+        { JSON.stringify( coords ) }
     </>
   )
 };
