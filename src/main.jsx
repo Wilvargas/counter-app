f13dfe13627916aa762a38b0f1a4d67d423d2d7f
Wilfredo <wilvargas7@gmail.com>
2024-08-22 23:55:26 -0500

    CallbackHook, y ShowIncrement

--- src/main.jsx
+++ src/main.jsx
@@ -13,12 +13,13 @@ import { createRoot } from 'react-dom/client';
 // import { FocusScreen } from './04- useRef/FocusScreen';
 // import { ResizeComponent } from './05-useLayoutEffect/ResizeComponent';
 // import { Memorize } from './06-memos/Memorize';
-import { MemoHook } from './06-memos/MemoHook';
+// import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
 
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MemoHook />
+    <CallbackHook />
   </StrictMode>,
 )
